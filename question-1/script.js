d89467035c7d72993662be7141d2b8373c87ab0b
gsuruguay <guillermo.suruguay@gmail.com>
2022-02-14 01:03:05 -0300

    Se crea funcion para resolver ejercicio 1

--- question-1/script.js
+++ question-1/script.js
@@ -1,8 +1,21 @@
 /**
  * On this module you should write your answer to question #1.
- * This file would be executed with the following command (N=100):
- * $ node scritp.js 100
+ * This file would be executed with:
+ * $ node scritp.js
  */
 
-const args = process.argv.slice(-1);
-console.log(`Running question #1 with args ${args}`)
+function cantidadFormasParaSubir(cantidadEscalones){
+    if (cantidadEscalones < 0) {
+        console.log(`No puedes introducir escalones con números negativos`);
+    }else{
+        let formasSubir = (cantidadEscalones < 3) ? cantidadEscalones : (cantidadEscalones-1) + (cantidadEscalones-2);
+        console.log(`Hay ${formasSubir} formas de subir la escalera de ${cantidadEscalones} escalones`);
+    }
+}
+
+cantidadFormasParaSubir(15);
+cantidadFormasParaSubir(-4);
+cantidadFormasParaSubir(2);
+cantidadFormasParaSubir(100);
+cantidadFormasParaSubir(1);
+cantidadFormasParaSubir(0);
